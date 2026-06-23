a32a98b6e63030fffe35ee21028955ba562bceaf
akshat-chore <choreakshat@gmail.com>
2026-06-23 16:48:24 +0530

    chore: project scaffold with POM folder structure and Playwright config

---
+++ utils/testData.ts
@@ -0,0 +1,12 @@
+export const testData = {
+  validUser: 'standard_user',
+  lockedUser: 'locked_out_user',
+  password: 'secret_sauce',
+  baseURL: 'https://www.saucedemo.com',
+
+  checkout: {
+    firstName: 'Test',
+    lastName: 'User',
+    postalCode: '411001',
+  },
+};
